e421d39cdf90ea75f6203bf06d9255778354181b
Cheri Allen <cherimarie@gmail.com>
2019-03-09 09:10:47 -0800

    File shuffle

---
+++ scripts/conjugation.js
@@ -0,0 +1,22 @@
+// <table>
+//   <thead>
+//     <tr>
+//       <th colspan="2">Camin<em>ar</em></th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//   </tbody>
+// </table>
+console.log("vamos!")
